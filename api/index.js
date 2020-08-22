e9d4367d4b66ebcb06e595fe4ae242e25edd5283
diazjuanjo <diazjuanjose@icloud.com>
2020-08-22 12:57:53 -0300

    api funcionando

--- api/index.js
+++ api/index.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const fetch = require('node-fetch')
 
 const app = express()
 
 app.get('/api/search', (req, res)=>{
-  res.send('Hola')
+  const query = req.query.q
+  fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
+    .then((res)=>{
+      return res.json()
+    })
+    .then((json)=>{
+      const items = []
+          json.results.map(result => {
+              const item = {
+                  id: result.id,
+                  title: result.title,
+                  price: result.price,
+                  currency_id: result.currency_id,
+                  available_quantity: result.available_quantity,
+                  thumbnail: result.thumbnail,
+                  condition: result.condition,
+              }
+              items.push(item)
+          })
+      res.send(items)
+    })
 })
 
 app.listen(4000)
